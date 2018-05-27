a5174de87af605b9871bac3db7afad281f611fe1
Jim Harrison <jimharrison@users.noreply.github.com>
2018-05-26 22:30:54 -0500

    add basic placeholders and some styles

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 //Business Logic
-document.getElementByClass("bias").innerHTML = "Paragraph changed from JS. <br> Hi Jim!";
-$(".bias").append("Hey Mike");
-
-//UI
-$(function () {
-  if ($("#last-price") > $("#prev-last-price")) {
-    $("#panel-bias").addClass("panel-danger")
-  } else if ($("#prev-last-price") > $("#last-price")) {
-    $("#panel-bias").addClass("panel-success")
-  } else {
-
-  }
-
-})
+document.getElementById("test-class").innerHTML = "Paragraph changed from JS. <br> Hi Jim!";
+$("#test-class").append("<br>Whaaaaazzzzuuuppp");
+//
 
 
+// // //UI
+// // expecting var's upDay and downDay from the function. (i used html ID's just as interim place holder
+// // will change bootstrap panel color from grey to green or red based on an up or down day
+
+// $(function () {
+  // if ($("#last-price") > $("#prev-last-price")) { // down day
+//     $("#panel-bias").removeClass("panel-default")
+//     $("#panel-bias").addClass("panel-danger") 
+//   } else if ($("#last-price") < $("#prev-last-price")) { //up day
+//     $("#panel-bias").removeClass("panel-default")
+//     $("#panel-bias").addClass("panel-success") 
+//   }
+//  
+// });
