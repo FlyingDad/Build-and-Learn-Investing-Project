5d5a11c4921228788e8da144a5182dde360ac8c2
Jim Harrison <jimharrison@users.noreply.github.com>
2018-05-26 20:57:59 -0500

    add basic form, simple css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-document.getElementById("test-class").innerHTML = "Paragraph changed from JS. <br> Hi Jim!";
+//Business Logic
+document.getElementByClass("bias").innerHTML = "Paragraph changed from JS. <br> Hi Jim!";
+$(".bias").append("Hey Mike");
+
+//UI
+$(function () {
+  if ($("#last-price") > $("#prev-last-price")) {
+    $("#panel-bias").addClass("panel-danger")
+  } else if ($("#prev-last-price") > $("#last-price")) {
+    $("#panel-bias").addClass("panel-success")
+  } else {
+
+  }
+
+})
+
+
