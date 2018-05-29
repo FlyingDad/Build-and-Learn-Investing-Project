7ffab7ffd0af3921adb7cc48058a29e024e66180
Jim Harrison <jimharrison@users.noreply.github.com>
2018-05-28 19:29:13 -0500

    add final field

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 //Business Logic
 document.getElementById("test-class").innerHTML = "Paragraph changed from JS. <br> Hi Jim!";
 $("#test-class").append("<br>Whaaaaazzzzuuuppp");
-let dayType = "downDay";  //will let me style things, add icons etc. based on last price either being > or < prev last price. Can you pass me this please :)  
+let dayType = "upDay";  //will let me style things, add icons etc. based on last price either being > or < prev last price. Can you pass me this please :)  
 
 
 //UI
 $("form#selector").submit(function (event) {
   event.preventDefault();
   let userInput = $("select#user-input").val();
+  
+  //check for valid input
   if (userInput !== "none") {
     $(".panel-body").show();
   } else {
@@ -23,6 +25,6 @@ $("form#selector").submit(function (event) {
     $("#panel-bias").removeClass("panel-default")
     $("#panel-bias").addClass("panel-success") //up day
   } else {
-
+    //unchanged
   }
 });
