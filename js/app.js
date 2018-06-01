d56bb2997dd9647610d5b49ee0a06b493fb0709c
Jim Harrison <jimharrison@users.noreply.github.com>
2018-06-01 13:44:53 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 /* jshint esversion: 6 */
 
 //Business Logic
-document.getElementById("test-class").innerHTML = "Paragraph changed from JS. <br> Hi Jim!";
-$("#test-class").append("<br>Whaaaaazzzzuuuppp");
-$("#test-class").append("<br>Nothin', just watching the code, having a Bud!");
-let dayType = "upDay";  //will let me style things, add icons etc. based on last price either being > or < prev last price. Can you pass me this please :)  
+
 
 // Gets date n days earlier
 Date.prototype.subtractDays = function (n) {
@@ -22,7 +19,7 @@ $("form#selector").submit(function (event) {
 	console.log(userInput);
   //check for valid input
   if (userInput !== "none") {
-		$(".panel-body").show();
+		$(".panel-body").slideDown();
 		switch (userInput){
 			case 'gold':
 				getUserSlected(goldUrl);
@@ -37,18 +34,6 @@ $("form#selector").submit(function (event) {
   } else {
     $(".panel-body").hide();
   }
-
-
-// testing up day down day
-  if (dayType === "downDay") {
-    $("#panel-bias").removeClass("panel-default");
-    $("#panel-bias").addClass("panel-danger"); //down day
-  } else if (dayType === "upDay") {
-    $("#panel-bias").removeClass("panel-default");
-    $("#panel-bias").addClass("panel-success"); //up day
-  } else {
-    //unchanged
-  }
 });
 
 // Mikes Code Below
@@ -60,6 +45,28 @@ class Bullion {
 	constructor(name, priceData){
 		this.name = name;   //Gold, Silver, etc
 		this.priceData = priceData;	 // array of 90 days of bullion prices
+	}
+
+	get sma5Day(){
+		return this.calcSma(5);   // 5 days
+	}
+
+	get sma20Day(){
+		return this.calcSma(20); // 20 days
+	}
+
+	get sma50Day(){
+		return this.calcSma(50); // 20 days
+	}
+
+	calcSma(days){
+		// calculate sma's here
+		//get last 'days' closing
+		let sum = 0;
+		for(let i = 0; i < days; i++){
+			sum += this.priceData[i][2]; // 2nd element in each day is closing price
+		}
+		return sum / days;
 	}
 }
 
@@ -80,8 +87,8 @@ function getData(url){
   .then(status)
   .then(json)
   .then(function(data) {
-		console.log('Request succeeded with JSON response', data);
-		console.table(data);
+		//console.log('Request succeeded with JSON response', data);
+		//console.table(data);
 		return data;
   }).catch(function(error) {
     console.log('Request failed', error);
@@ -102,13 +109,17 @@ function getUserSlected(selected){
 	getBullion(selected + getDateStamp())
 	.then(bullion=>{
 	//will send to HTML here
-	console.table(bullion);
+	//console.table(bullion);
 	document.getElementById('time-stamp').innerHTML = bullion.priceData[0][0];
 	document.getElementById('description').innerHTML = bullion.name;
-	document.getElementById('open-price').innerHTML = 'Open: $' + bullion.priceData[0][1];
-	document.getElementById('close-price').innerHTML = 'Close: $' + bullion.priceData[0][2];
-	//console.table(bullion.priceData);
+	document.getElementById('open-price').innerHTML = `Open: $${bullion.priceData[0][1]}`;
+	document.getElementById('close-price').innerHTML = `Close: $' ${bullion.priceData[0][2]}`;
+	document.getElementById('sma-5day').innerHTML = `5 Day SMA: ${bullion.sma5Day.toFixed(2)}`;
+	document.getElementById('sma-20day').innerHTML = `20 Day SMA: ${bullion.sma20Day.toFixed(2)}`;
+	document.getElementById('sma-50day').innerHTML = `50 Day SMA: ${bullion.sma50Day.toFixed(2)}`;
+	calculateSMABias(bullion);
 	});
+	
 }
 
 function getDateStamp(){
@@ -119,3 +130,41 @@ function getDateStamp(){
 	let day = previousDate.getDay();
 	return `${year}-${month}-${day}`
 }
+
+function calculateSMABias(bullion){
+	let sma5 = bullion.sma5Day;
+	let sma20 = bullion.sma20Day;
+	let sma50 = bullion.sma50Day;
+	let last = bullion.priceData[0][1]; //last price, settle or close value using
+	let priorLast = bullion.priceData[1][1]; // prior last or....
+	$(".bias").append(`<p>Yest Close: ${last}</p>`)
+	
+	if (last > priorLast) {
+		$("#panel-bias").removeClass("panel-default");
+		$("#panel-bias").addClass("panel-success"); //up day
+
+	} else if (last < priorLast){	
+		$("#panel-bias").removeClass("panel-default");
+    $("#panel-bias").addClass("panel-danger"); //down day
+	} else {
+		//unchanged
+	}
+
+	let bias = "";
+	let mom1 = (sma5 - last).toFixed(2); 
+	let mom2 = (sma20 - last).toFixed(2);
+	let mom3 = (sma50 - last).toFixed(2);
+	$(".bias").append(`<p>Mom 1 ${mom1}</p>`)
+	$(".bias").append(`<p>Mom 2 ${mom2}</p>`)
+	$(".bias").append(`<p>Mom 3 ${mom3}</p>`)
+	
+	if (mom1 >=0 && mom2 >=0 && mom3 >= 0) {
+		bias = "BUY"
+	} else if (mom1 <=0 && mom2 <=0 && mom3 <= 0) {
+		bias = "SELL"
+	} else {
+		bias = "NONE"
+	}
+	$(".bias").append(`<p>Bias is: ${bias}</p>`)
+}
+
