af58b2853f455ae7bc4b88fa4997d45f608ec889
Jim Harrison <jimharrison@users.noreply.github.com>
2018-07-24 16:35:51 -0500

    yep

--- js/app.js
+++ js/app.js
@@ -235,31 +235,31 @@ function calculateSMABias() {
 	$("#c-change").html(`Change: $${change.toFixed(2)}`);
 
 	if (change > 0) {
-		$(".panel-bias").removeClass("panel-default");
-		$(".panel-bias").removeClass("panel-danger");
-		$(".panel-bias").addClass("panel-success"); //up day
-		// $(".bias-smile").html(`<i class="fas fa-smile fa-3x text-success"></i> `); //up day
-		// $(".bias-smile").append(`<i class="fas fa-meh fa-3x text-default"></i> `);
+	// 	$(".panel-bias").removeClass("panel-default");
+	// 	$(".panel-bias").removeClass("panel-danger");
+	// 	$(".panel-bias").addClass("panel-success"); //up day
+	// 	// $(".bias-smile").html(`<i class="fas fa-smile fa-3x text-success"></i> `); //up day
+	// 	// $(".bias-smile").append(`<i class="fas fa-meh fa-3x text-default"></i> `);
 		$("#c-change").removeClass("fa-sort-amount-down");
 		$("#c-change").append(` <i class="fas fa-sort-amount-up" style="color:green"></i>`);
-		// $("ul#bias").append(`<li><h2>Todays Projected High: ${fibPredictedHigh.toFixed(2)}</h2></li>`);
+	// 	// $("ul#bias").append(`<li><h2>Todays Projected High: ${fibPredictedHigh.toFixed(2)}</h2></li>`);
 
 
 	} else if (change < 0) {
-		$(".panel-bias").removeClass("panel-default");
-		$(".panel-bias").removeClass("panel-success");
-		$(".panel-bias").addClass("panel-danger"); //down day
-		// $(".bias-smile").html(`<i class="fas fa-frown fa-3x text-danger"></i>`);
-		// $(".bias-smile").append(`<i class="fas fa-meh fa-3x text-default"></i>`);
+	// 	$(".panel-bias").removeClass("panel-default");
+	// 	$(".panel-bias").removeClass("panel-success");
+	// 	$(".panel-bias").addClass("panel-danger"); //down day
+	// 	// $(".bias-smile").html(`<i class="fas fa-frown fa-3x text-danger"></i>`);
+	// 	// $(".bias-smile").append(`<i class="fas fa-meh fa-3x text-default"></i>`);
 		$("#c-change").removeClass("fa-sort-amount-up");
 		$("#c-change").append(` <i class="fas fa-sort-amount-down" style="color:red"></i>`);
-		// $("ul#bias").append(`<li><h2>Todays Projected Low: ${fibPredictedLow.toFixed(2)}</h2></li>`);
+	// 	// $("ul#bias").append(`<li><h2>Todays Projected Low: ${fibPredictedLow.toFixed(2)}</h2></li>`);
 
 	} else {
-		$(".panel-bias").removeClass("panel-success");
-		$(".panel-bias").removeClass("panel-danger");
-		$(".panel-bias").addClass("panel-default"); //unchanged
-		// $(".bias-smile").html(`<br><i class="fas fa-meh fa-3x text-default text-center"></i>`);
+	// 	$(".panel-bias").removeClass("panel-success");
+	// 	$(".panel-bias").removeClass("panel-danger");
+	// 	$(".panel-bias").addClass("panel-default"); //unchanged
+	// 	// $(".bias-smile").html(`<br><i class="fas fa-meh fa-3x text-default text-center"></i>`);
 	}
 
 	let bias = "";
